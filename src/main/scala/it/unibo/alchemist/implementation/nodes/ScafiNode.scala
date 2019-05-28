f9eb12840f59deb90b177c2cd9118fa2ea89c8c8
Roberto Casadei <robyonrails@gmail.com>
2019-05-28 17:29:55 +0200

    ScafiNode returning null for missing molecules

---
+++ src/main/scala/it/unibo/alchemist/implementation/nodes/ScafiNode.scala
@@ -0,0 +1,10 @@
+package it.unibo.alchemist.implementation.nodes
+
+import it.unibo.alchemist.model.implementations.nodes.AbstractNode
+import it.unibo.alchemist.model.interfaces.Environment
+
+class ScafiNode(env: Environment[_, _]) extends AbstractNode[Any](env) {
+
+  override def createT = null
+
+}
